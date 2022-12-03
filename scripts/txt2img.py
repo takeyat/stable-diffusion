0f2cb57d891c623e3da5df6a7b58e209922bc2d0
takeyat <machshev1969@gmail.com>
2022-12-04 01:06:55 +0900

    local change

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -104,6 +104,9 @@ def check_safety(x_image):
             x_checked_image[i] = load_replacement(x_checked_image[i])
     return x_checked_image, has_nsfw_concept
 
+def null_safety(x_image):
+    return x_image, []
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -321,7 +324,8 @@ def main():
                         x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
                         x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
 
-                        x_checked_image, has_nsfw_concept = check_safety(x_samples_ddim)
+                        # x_checked_image, has_nsfw_concept = check_safety(x_samples_ddim)
+                        x_checked_image, has_nsfw_concept = null_safety(x_samples_ddim)
 
                         x_checked_image_torch = torch.from_numpy(x_checked_image).permute(0, 3, 1, 2)
 
